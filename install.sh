bf482e4eaad31bb203c7accee086c5dc118fe04b
SamiKO228 <104057661+SamiKO228@users.noreply.github.com>
2026-05-14 19:50:53 +0300

    Filling content of install.sh
    code to automate installation of the watchdog, without flexibility to consider the path to files

--- install.sh
+++ install.sh
@@ -1,1 +1,28 @@
+#!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root (use sudo)"
+   exit 1
+fi
+
+echo "Installing Process Watchdog..."
+
+# 1. Create needed folders
+mkdir -p /etc/watchdog
+touch /var/log/watchdog.log
+touch /var/log/watchdog_alerts.log
+
+# 2. Copy the files (considering you're in root)
+cp watchdog.sh /usr/local/bin/
+cp watchdog.conf /etc/watchdog/
+cp watchdog.service /etc/systemd/system/
+
+# 3. Set the rights
+chmod +x /usr/local/bin/watchdog.sh
+
+# 4. Restart the systemd daemon and start the service
+systemctl daemon-reload
+systemctl enable watchdog.service
+systemctl restart watchdog.service
+
+echo "Watchdog installed and started! Check status: systemctl status watchdog"
